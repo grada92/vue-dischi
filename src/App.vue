f7499f3df95b363d5181cd64af728af12fcbd031
grada92 <danielegradassai@gmail.com>
2022-09-26 15:52:58 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+import HeaderComponent from './components/HeaderComponent.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+  },
+  data() {
+    return {
+      cdCard : [],
+    }
+  },
+  created() {
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/array/music")
+    .then((response) => {
+      console.log(response)
+      this.cdCard = response.data.response;
+
+    })
+    
+    
+  }
+  
+  
+
+
+}
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+   
+     <h3>ciao</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'HeaderComponent',
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    background-color:#2e3a46;
+    height: 50px;
+   
+}
+</style>
