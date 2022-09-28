da535142ccfe23ecf990a14c4b4d8559af29e28b
grada92 <danielegradassai@gmail.com>
2022-09-28 13:02:30 +0200

    end project

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,32 +1,81 @@
 <template>
     <main>
+        <!--richiamo dati da SelectAreaComponent con @selected-->
+        <SelectAreaComponent :options="genres" @selected="genreSelected" /> <!--Creo funzione per utilizzare i parametri-->
         <div class="container row">
-            <div class="d-flex flex-column justify-content-between card" v-for="cd in cdList" :key="cd.title" >
-            <div class="box-card">
-                <img :src="cd.poster" alt="">
+
+                                        <!--Sostituisco cdList con FilteredArray per passare i valori selezionati-->
+            <div class="d-flex flex-column justify-content-between card" v-for="cd in filteredArray" :key="cd.title">
+                <div class="box-card">
+                    <img :src="cd.poster" alt="">
+                </div>
+                <h5>{{ cd.title }}</h5>
+                <h6>{{cd.author}} <h6>{{cd.year}}</h6></h6>
+                
+
+
             </div>
-            <h5>{{ cd.title }}</h5>
-            <h6>{{cd.author}} <h6>{{cd.year}}</h6></h6>
-            
-
 
         </div>
-
-
-
-        </div>
-        
-
     </main>
   
 </template>
 
 <script>
+
+import SelectAreaComponent from './SelectAreaComponent.vue';
+
+
 export default {
 name : 'MainComponent',
 props : {
     cdList : Array,
-}
+},
+data(){
+    return {
+        selected:'',
+    }
+},
+computed:{
+    genres(){
+        const array = [];
+        this.cdList.forEach(element => {
+            if(!array.includes(element.genre)){
+                array.push(element.genre);
+            }
+            
+        });
+       console.log({genres: array})
+        return array;
+    },
+    filteredArray(){  // Creo Array vuoto e per ogni elemento inserisco nel FilteredArray i generi selezionati
+        let array = [];
+        this.cdList.forEach(item =>{   // cdList Array Totale
+            if(item.genre === this.selected){
+                array.push(item)
+            }
+            if(this.selected === ''){
+                array = this.cdList  // Nuovo Array = FilteredArray
+            }
+        })
+        return array
+    }
+    
+   
+
+},
+components: {
+    SelectAreaComponent,
+    
+},
+methods: {
+    genreSelected(genres){
+        this.selected = genres  //la funzione raccoglie i valori di @selected e li metto in selected=''
+
+
+    }
+        
+},
 }
 </script>
 
---
+++ src/components/SelectAreaComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <select name="Select-area" v-model="genres" @change="changeSelect">
+        <option value="">All Genres</option>
+        <option v-for="option in options" :key="option" :value="option">
+            {{option}}
+        </option>
+        
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'SelectAreaComponent',
+data(){
+    return {
+        genres: ''
+    }
+  
+},
+props: {
+    options: Array
+
+},
+methods: {
+    changeSelect(){
+        console.log(this.genres)
+        this.$emit('selected', this.genres); // Passo gli Elementi a MainComponent con $emit
+    }
+}
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
